931a18ccbfadc2393014a01628b596fb565d7085
Tareq Assi <42150220+ZF7724@users.noreply.github.com>
2020-05-19 15:30:43 -0400

    Update Dictionary App Code v2.py

--- Dictionary App Code v2.py
+++ Dictionary App Code v2.py
@@ -7,10 +7,10 @@ from difflib import get_close_matches as GCM
 
 connect=mysql.connector.connect(
         
-        user="ardit700_student",
-        password="ardit700_student",
-        host="108.167.140.122",
-        database="ardit700_pm1database"
+        user="***********",
+        password="********",
+        host="***********",
+        database="*************"
         
         
         )
