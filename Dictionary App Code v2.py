16ea76bf3cd3bfcc9a18bf80f51e4a825301a1ba
Tareq Assi <42150220+ZF7724@users.noreply.github.com>
2020-05-19 15:07:12 -0400

    Add files via upload
    (Integrated mySQL, no data needed on laptop. Everything extracted from servers)

---
+++ Dictionary App Code v2.py
@@ -0,0 +1,160 @@
+import mysql.connector
+import difflib
+from difflib import SequenceMatcher as SM
+from difflib import get_close_matches as GCM
+
+
+
+connect=mysql.connector.connect(
+        
+        user="ardit700_student",
+        password="ardit700_student",
+        host="108.167.140.122",
+        database="ardit700_pm1database"
+        
+        
+        )
+
+def Convert(tup, di): 
+    for a, b in tup: 
+        di.setdefault(a, []).append(b) 
+    return di 
+    
+
+def define(word):
+    
+    Cursor=connect.cursor()
+    
+    Query=Cursor.execute("SELECT * FROM Dictionary")
+    
+    Result=Cursor.fetchall()
+    
+    dictionary={}
+    
+    Result=Convert(Result,dictionary)
+    
+
+    
+    if word in Result:
+        definition=Result[word]
+        return definition
+    
+    elif word.upper() in Result:
+        definition=Result[word.upper()]
+        return definition
+    
+    elif word.title() in Result:
+        definition=Result[word.title()]
+        return definition
+    
+        
+    elif len(GCM(word,Result.keys(),cutoff=0.8))>0 :
+        correction=input("Did you mean %s instead? Enter Y if yes, or N if no: " % GCM(word,Result.keys(),cutoff=0.8)[0])
+        
+        correction=correction.upper()
+        
+        if correction=="Y":
+            return Result[GCM(word,Result.keys(),cutoff=0.8)[0]]
+            
+        elif correction=="N":
+            return "Word does not exist. Please double check word."
+            
+        else:
+            return "We did not understand your entry."
+        
+
+    
+def main():
+    
+    word=input("Enter a word: ")
+    definition=define(word)
+    print(definition)
+        
+main()
+        
+        
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
